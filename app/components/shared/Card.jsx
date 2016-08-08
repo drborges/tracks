88508ebec971758b75f876095d626f1b8c7dbe9f
Diego Borges <drborges.cic@gmail.com>
2016-08-08 14:39:53 -0300

    Add UI state to the card component

--- app/components/shared/Card.jsx
+++ app/components/shared/Card.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import { markdown } from 'markdown'
 
-const Card = (props) => (
-  <li className="card" draggable="true">
-    <div className="header">{props.issue.id}</div>
-    <div className="body" dangerouslySetInnerHTML={{ __html: markdown.toHTML(props.issue.name) }} />
-    <div className="footer"></div>
-  </li>
-)
+class Card extends React.Component {
+  state = { editing: false }
+
+  render() {
+    let bodyHTML = this.state.editing ? this.props.issue.name : markdown.toHTML(this.props.issue.name)
+
+    return (
+      <li className="card" draggable="true">
+        <div className="header">{this.props.issue.id}</div>
+        <div
+          className="body"
+          contentEditable="true"
+          onFocus={() => this.setState({ editing: true})}
+          onBlur={()  => this.setState({ editing: false})}
+          dangerouslySetInnerHTML={{ __html: bodyHTML }} />
+        <div className="footer"></div>
+      </li>
+    )
+  }
+}
 
 export default Card
