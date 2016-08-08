479840c9c3bc3848ad09b76a68f6f7352b61a8ec
Diego Borges <drborges.cic@gmail.com>
2016-08-08 09:57:36 -0300

    Render story name as markdown

--- app/components/shared/Card.jsx
+++ app/components/shared/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { markdown } from 'markdown'
 
 const Card = (props) => (
   <li className="card" draggable="true">
     <div className="header">{props.issue.id}</div>
-    <div className="body">{props.issue.name}</div>
+    <div className="body" dangerouslySetInnerHTML={{ __html: markdown.toHTML(props.issue.name) }} />
     <div className="footer"></div>
   </li>
 )
